a7eaa7096b10133e184f72156ac1cc9fc17b6fc8
ll574918628 <574918628@qq.com>
2023-03-29 10:48:15 +0800

    update

--- scripts/cfg/flow_cfg.py
+++ scripts/cfg/flow_cfg.py
@@ -17,7 +17,7 @@ from   data_def import *
 
 asic_top        = Flow('asic_top','smic110','HD','MAX','iEDA')
 aes             = Flow('aes_cipher_top','sky130','HS','TYP','')
-gcd             = Flow('gcd','sky130','HS','TYP','iEDA')
+gcd             = Flow('gcd','sky130','HS','TYP','')
 uart            = Flow('uart','asap7','HS','TYP','')
 ibex            = Flow('ibex_core','sky130','HS','TYP','')
 picorv32        = Flow('picorv32','sky130','HS','TYP','')
