4e22259e3cd40b05143eb507c383d690e91ad78e
ll574918628 <574918628@qq.com>
2023-04-10 17:45:36 +0800

    update iEDA lib

--- build_iflow.sh
+++ build_iflow.sh
@@ -59,3 +59,6 @@ RUN git pull origin master
 
 # install tools
 RUN $IFLOW_SHELL_DIR/install_tools.sh
+
+# link iEDA lib
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:'$IFLOW_TOOLS_DIR'/iEDA_0.1/lib' >> ~/.bashrc
